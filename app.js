c4b855550ae6723e2c8e869366f86aeeb15f158a
kenchen64 <kenchen6464@gmail.com>
2026-04-26 20:02:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('client'));
 
 // ===== MongoDB =====
 mongoose.connect(process.env.MONGO_URI);
